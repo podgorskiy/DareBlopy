4d73e9c626f6efb1d2ba11aa6da3a78146456d26
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-10 08:14:18 -0400

    Added license Added indication of running debug session Updated fsal

--- dareblopy/__init__.py
+++ dareblopy/__init__.py
@@ -1,6 +1,30 @@
+# Copyright 2019-2020 Stanislav Pidhorskyi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+import os
 import sys
-# sys.path.insert(0, "cmake-build-debug/")
-# sys.path.insert(0, "cmake-build-release/")
+
+# if running debug session
+if os.path.exists("cmake-build-debug/"):
+    print('Running Debugging session!')
+    sys.path.insert(0, "cmake-build-debug/")
+    # sys.path.insert(0, "cmake-build-release/")
+
+del os
+del sys
 
 from _dareblopy import *
 

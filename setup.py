c78d15193d063bf54ad2d627ea4234820f17add4
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-11 04:52:24 -0400

    Removed headers from sources since distutils seems to be confused with that

--- setup.py
+++ setup.py
@@ -181,9 +181,7 @@ zlib = list(glob.glob('libs/zlib/*.c'))
 lz4 = list(glob.glob('libs/lz4/lib/*.c'))
 dareblopy = list(glob.glob('sources/*.c*')) + list(glob.glob('sources/protobuf/*.c*'))
 
-crc32c = """crc32c.cc crc32c_arm64.cc crc32c_arm64.h crc32c_arm64_linux_check.h
-        crc32c_internal.h crc32c_portable.cc crc32c_prefetch.h crc32c_read_le.h crc32c_round_up.h crc32c_sse42.cc
-        crc32c_sse42.h crc32c_sse42_check.h"""
+crc32c = """crc32c.cc crc32c_arm64.cc crc32c_portable.cc crc32c_sse42.cc"""
 
 crc32c = ['libs/crc32c/src/' + x for x in crc32c.split()]
 
@@ -223,15 +221,15 @@ jpeg_turbo = ['libs/libjpeg-turbo/' + x for x in jpeg_turbo.split()]
 
 
 jpeg_turbo_simd = """x86_64/jsimdcpu.asm x86_64/jfdctflt-sse.asm
-    x86_64/jccolor-sse2.asm x86_64/jcgray-sse2.asm x86_64/jchuff-sse2.asm
-    x86_64/jcphuff-sse2.asm x86_64/jcsample-sse2.asm x86_64/jdcolor-sse2.asm
-    x86_64/jdmerge-sse2.asm x86_64/jdsample-sse2.asm x86_64/jfdctfst-sse2.asm
-    x86_64/jfdctint-sse2.asm x86_64/jidctflt-sse2.asm x86_64/jidctfst-sse2.asm
-    x86_64/jidctint-sse2.asm x86_64/jidctred-sse2.asm x86_64/jquantf-sse2.asm
-    x86_64/jquanti-sse2.asm
-    x86_64/jccolor-avx2.asm x86_64/jcgray-avx2.asm x86_64/jcsample-avx2.asm
-    x86_64/jdcolor-avx2.asm x86_64/jdmerge-avx2.asm x86_64/jdsample-avx2.asm
-    x86_64/jfdctint-avx2.asm x86_64/jidctint-avx2.asm x86_64/jquanti-avx2.asm x86_64/jsimd.c"""
+        x86_64/jccolor-sse2.asm x86_64/jcgray-sse2.asm x86_64/jchuff-sse2.asm
+        x86_64/jcphuff-sse2.asm x86_64/jcsample-sse2.asm x86_64/jdcolor-sse2.asm
+        x86_64/jdmerge-sse2.asm x86_64/jdsample-sse2.asm x86_64/jfdctfst-sse2.asm
+        x86_64/jfdctint-sse2.asm x86_64/jidctflt-sse2.asm x86_64/jidctfst-sse2.asm
+        x86_64/jidctint-sse2.asm x86_64/jidctred-sse2.asm x86_64/jquantf-sse2.asm
+        x86_64/jquanti-sse2.asm
+        x86_64/jccolor-avx2.asm x86_64/jcgray-avx2.asm x86_64/jcsample-avx2.asm
+        x86_64/jdcolor-avx2.asm x86_64/jdmerge-avx2.asm x86_64/jdsample-avx2.asm
+        x86_64/jfdctint-avx2.asm x86_64/jidctint-avx2.asm x86_64/jquanti-avx2.asm x86_64/jsimd.c"""
 
 jpeg_turbo_simd = ['libs/libjpeg-turbo/simd/' + x for x in jpeg_turbo_simd.split()]
 
